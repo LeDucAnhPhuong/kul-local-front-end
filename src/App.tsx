ad63cb2dfb25463f381f3db33d3216d1a66cb03a
sawsew467 <thangtvb.dev@gmail.com>
2025-05-16 16:50:15 +0700

    init shadcn with vite

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;

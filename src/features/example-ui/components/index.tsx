00067b71a38e8186fe2efab49239bc5b8c19dcc5
Lê Đức Anh Phương <leducanhphuongdev@gmail.com>
2025-05-22 11:42:17 +0700

    example ui

--- src/features/example-ui/components/index.tsx
+++ src/features/example-ui/components/index.tsx
@@ -1,8 +1,9 @@
 import DataTable from '@/components/data-table/data-table';
 import TitlePage from '@/components/ui/title-page';
 import React from 'react';
-import { exampleUiSampleData } from '../data.example-ui';
+import { exampleScheduelData, exampleUiSampleData } from '../data.example-ui';
 import { columns } from '../columns/example-UI';
+import { columns as columnsSchedule } from '../columns/schedule.comlumns';
 import DataCard from '@/components/ui/data-card';
 
 const ExampleUI = () => {
@@ -10,6 +11,7 @@ const ExampleUI = () => {
     <div className="bg-white dark:bg-background p-4 rounded-xl border-[1px] border-stone-50 dark:border-stone-800">
       <TitlePage title="example" href="/student-management" contentHref="Student Management" />
       <DataTable data={exampleUiSampleData} columns={columns} />
+      <DataTable data={exampleScheduelData} columns={columnsSchedule} />
       {/* <DataCard data={exampleUiSampleData} columns={columns} /> */}
     </div>
   );

ba42de5d0f83ca7792d898d831afafe29e2fc55b
Lê Đức Anh Phương <127871368+LeDucAnhPhuong@users.noreply.github.com>
2025-07-24 15:08:21 +0700

    Feat/docker (#26)
    * update-room-management
    
    * fix lai theo data
    
    * đổi lại tên
    
    * sua lai news
    
    * them academic progress
    
    * xong roi do
    
    * done docker
    
    * fix build fail production
    
    * toi uu build
    
    ---------
    
    Co-authored-by: Binh <hoangcongbinh121104@gmail.com>
    Co-authored-by: sawsew467 <thangtvb.dev@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,41 @@
-import path from "path";
-import { defineConfig } from "vite";
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    sourcemap: false, // giảm kích thước build
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor_react';
+            if (id.includes('quill')) return 'vendor_quill';
+            if (id.includes('lodash')) return 'vendor_lodash';
+            if (id.includes('framer-motion')) return 'vendor_motion';
+            if (id.includes('@radix-ui')) return 'vendor_radix';
+            if (id.includes('zod')) return 'vendor_zod';
+            if (id.includes('date-fns')) return 'vendor_date';
+            if (id.includes('uuid')) return 'vendor_uuid';
+            if (id.includes('recharts')) return 'vendor_chart';
+            if (id.includes('axios')) return 'vendor_axios';
+            if (id.includes('react-router')) return 'vendor_router';
+            if (id.includes('clsx')) return 'vendor_clsx';
+            if (id.includes('html-to-text')) return 'vendor_html';
+            if (id.includes('@dnd-kit')) return 'vendor_dnd';
+            if (id.includes('@reduxjs')) return 'vendor_redux';
+            if (id.includes('react-hook-form')) return 'vendor_rhf';
+            return 'vendor_misc'; // phần còn lại chưa bắt được
+          }
+        },
+      },
     },
   },
 });

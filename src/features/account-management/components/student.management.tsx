8cd765b4d156198bdbd088d22433c19d25d9a12f
Lê Đức Anh Phương <127871368+LeDucAnhPhuong@users.noreply.github.com>
2025-07-25 12:17:06 +0700

    Feat/docker (#29)
    * update-room-management
    
    * fix lai theo data
    
    * đổi lại tên
    
    * sua lai news
    
    * them academic progress
    
    * xong roi do
    
    * done docker
    
    * fix build fail production
    
    * toi uu build
    
    * change dockerfile
    
    * fix error in production
    
    * fix error name in account management
    
    ---------
    
    Co-authored-by: Binh <hoangcongbinh121104@gmail.com>
    Co-authored-by: sawsew467 <thangtvb.dev@gmail.com>

--- src/features/account-management/components/student.management.tsx
+++ src/features/account-management/components/student.management.tsx
@@ -11,8 +11,8 @@ const StudentManagement = () => {
         data?.data?.map((item: StudentData) => ({
           ...item,
           name:
-            item.first_name || item.last_name
-              ? `${item.last_name ?? ''} ${item.first_name ?? ''}`
+            item.firstName || item.lastName
+              ? `${item.lastName ?? ''} ${item.firstName ?? ''}`
               : 'N/A',
         })) || [],
       isFetching,
